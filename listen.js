bd0d4368a70fff5de8539f316b3c1932690fc921
howes <johnhowes93@googlemail.com>
2019-04-01 14:10:10 +0100

    git

--- listen.js
+++ listen.js
@@ -1,4 +1,3 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 const { PORT = 9090 } = process.env;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
