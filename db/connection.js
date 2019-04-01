60de2c9a40fb4dcb775622a5e28b8fbfdc1a7ea5
howes <johnhowes93@googlemail.com>
2019-04-01 10:32:50 +0100

    added files for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,11 @@
 // ./db/connection.js
 const knex = require('knex');
 const dbConfig = require('../knexfile');
+const ENV = process.env.NODE_ENV || 'development';
+const config =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile')[ENV];
 
 const connection = knex(dbConfig);
-module.exports = connection;
+module.exports = require('knex')(config);
