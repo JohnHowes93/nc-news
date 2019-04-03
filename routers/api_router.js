b1ec27b6019e3c6bed38fc1fa09571c5044443ae
howes <johnhowes93@googlemail.com>
2019-04-03 14:46:00 +0100

    added pagination, fixed an issue with comments returning incorrectly

--- routers/api_router.js
+++ routers/api_router.js
@@ -5,6 +5,7 @@ const commentsRouter = require('./comments_router');
 const usersRouter = require('./users_router');
 const sendApiInfo = require('../controllers/api');
 const { handle405 } = require('../errors/index');
+
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/articles', articlesRouter);
 apiRouter.use('/comments', commentsRouter);
